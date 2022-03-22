4d9ea737d0f24313bd3abb31137a1a65955b65a5
Nathan Clonts <nacclonts@gmail.com>
2022-03-22 07:51:53 -0600

    add simple encryption+decryption using key

--- alice.py
+++ alice.py
@@ -1,4 +1,7 @@
 from multiprocessing.connection import Listener
+import sys
+
+from encryptionlib import decrypt
 
 # public variables
 public_modulus_p = 23
@@ -6,21 +9,25 @@ public_base_g = 5
 
 alice_secret = 4
 alice_public = (public_base_g ** alice_secret) % public_modulus_p
+secret_key = None
 
 def listen(port):
     listener = Listener(('localhost', port))
     print('Alice listenting at port', port)
     conn = listener.accept()
-    print('Connection accepted:', listener.last_accepted)
     while True:
         msg = conn.recv()
-        if msg.startswith('public:'):
+        if str(msg).startswith('public:'):
             bob_public_key = int(msg.split('public:')[1])
             print('Alice received Bob\'s public key:', bob_public_key)
             conn.send('public:' + str(alice_public))
 
             secret_key = (bob_public_key ** alice_secret) % public_modulus_p
             print('Alice generated secret key:', secret_key)
+        else:
+            print('\n-- Alice received message: --')
+            print('    Ciphertext:', msg)
+            print('    Plaintext:', decrypt(msg, secret_key))
 
         if msg == 'close':
             conn.close()
--- bob.py
+++ bob.py
@@ -1,5 +1,7 @@
 from multiprocessing.connection import Client
 
+from encryptionlib import encrypt
+
 public_modulus_p = 23
 public_base_g = 5
 
@@ -8,6 +10,7 @@ bob_public = (public_base_g ** bob_secret) % public_modulus_p
 
 def begin(port):
     conn = Client(('localhost', port))
+    print('Bob connected to port', port)
     conn.send('public:' + str(bob_public))
     response = conn.recv()
     alice_public_key = int(response.split('public:')[1])
@@ -16,7 +19,12 @@ def begin(port):
     secret_key = (alice_public_key ** bob_secret) % public_modulus_p
     print('Bob generated secret key:', secret_key)
 
-    conn.send('close')
+    while True:
+        input_text = input('> ')
+        conn.send(encrypt(input_text, secret_key))
+        if input_text == 'close':
+            break
+
     conn.close()
 
 if __name__ == '__main__':
